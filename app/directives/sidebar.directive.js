49f93129b50afd8205b1cd4313a52b003e53ed32
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2017-02-08 02:32:15 +0200

    Made the sidebar into a directive

---
+++ app/directives/sidebar.directive.js
@@ -0,0 +1,35 @@
+(function() {
+    "use strict";
+
+    angular
+        .module("app")
+        .directive("sidebar", directive);
+
+    /* @ngInject */
+    function directive() {
+        var directive = {
+            restrict: "E",
+            replace: "true",
+            templateUrl: "/app/directives/sidebar.directive.html",
+            scope: {},
+            controller: Controller,
+            controllerAs: "vm",
+            bindToController: true
+        };
+
+        return directive;
+    }
+
+    Controller.$inject = [];
+
+    /* @ngInject */
+    function Controller($rootScope, localStorageService) {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+
+        }
+    }
+})();
