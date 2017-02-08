3b4b5e53f00b2c04f236bba4c97dcf986029d79e
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2017-02-08 15:51:43 +0200

    Init sidebar option functions

--- app/directives/sidebar.directive.js
+++ app/directives/sidebar.directive.js
@@ -26,32 +26,73 @@
     function Controller() {
         var vm = this;
 
-        vm.species = [
-              {
-                name: "mallard"
-              },
-              {
-                name: "redhead"
-              },
-              {
-                name: "gadwall"
-              },
-              {
-                name: "canvasback"
-              },
-              {
-                name: "lesser scaup"
-              }
-            ];
+        vm.initOptions = initOptions;
+        vm.updateOrder = updateOrder;
+        vm.updateSpeciesFilter = updateSpeciesFilter;
+        vm.updateMinBirdNum = updateMinBirdNum;
+        vm.updateMaxBirdNum = updateMaxBirdNum;
+
+        // Species filter init
+        var allSpecies = {name: "all"}
+        vm.species = [allSpecies]
+        vm.speciesFilter = vm.species[0];
 
         activate();
 
         function activate() {
-
+            getSpecies()
         }
 
-        function orderBy() {
+        function updateOrder() {
+            console.log(vm.order);
+        }
 
+        function updateSpeciesFilter() {
+            console.log(vm.speciesFilter);
+        }
+
+        function updateMinBirdNum() {
+            console.log(vm.minBirdNum);
+        }
+
+        function updateMaxBirdNum() {
+            console.log(vm.maxBirdNum);
+        }
+
+        function initOptions() {
+            vm.order = "0";
+            vm.speciesFilter = vm.species[0];
+            vm.minBirdNum = null;
+            vm.maxBirdNum = null;
+
+            vm.updateOrder();
+            vm.updateSpeciesFilter();
+            vm.updateMinBirdNum();
+            vm.updateMaxBirdNum();
+        }
+
+        function getSpecies() {
+            vm.species = [
+                  {
+                    name: "mallard"
+                  },
+                  {
+                    name: "redhead"
+                  },
+                  {
+                    name: "gadwall"
+                  },
+                  {
+                    name: "canvasback"
+                  },
+                  {
+                    name: "lesser scaup"
+                  }
+                ];
+
+            vm.species.unshift(allSpecies)
+
+            vm.initOptions();
         }
     }
 })();
